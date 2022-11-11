b8b6bd316c78b36d8a3906702dc19b5a7cffa75f
NDC114 <duyencuongmtpvip2001@gmail.com>
2022-11-11 16:39:11 +0700

    first commit

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -6,12 +6,14 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using NguyenDuyenCuongBTH3.Models;
+using NguyenDuyenCuongBTH3.Models.Process;
 
 namespace NguyenDuyenCuongBTH3.Controllers
 {
     public class EmployeeController : Controller
     {
         private readonly ApplicationContext _context;
+         private ExcelProcess _excelProcess = new ExcelProcess();
 
         public EmployeeController(ApplicationContext context)
         {
@@ -157,6 +159,48 @@ namespace NguyenDuyenCuongBTH3.Controllers
         private bool EmployeeExists(string id)
         {
           return (_context.Employee?.Any(e => e.EmpId == id)).GetValueOrDefault();
+        }
+          public async Task<IActionResult> Upload()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            if (file != null)
+            {
+                string fileExtension = Path.GetExtension(file.FileName);
+                if (fileExtension != ".xls" && fileExtension != ".xlsx")
+                {
+                    ModelState.AddModelError("", "Please choose excel file to upload!");
+                }
+                else
+                {
+                    var FileName = DateTime.Now.ToShortTimeString() + fileExtension;
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory() + "/Uploads/Excel", FileName);
+                    var fileLocation = new FileInfo(filePath).ToString();
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        //save file to sever
+                        await file.CopyToAsync(stream);
+                        var dt = _excelProcess.ExcelToDataTable(fileLocation);
+                        for (int i = 0; i < dt.Rows.Count; i++)
+                        {
+                            var emp = new Employee();
+
+                            emp.EmpId = dt.Rows[i][0].ToString();
+                            emp.EmpName = dt.Rows[i][1].ToString();
+                            emp.Address = dt.Rows[i][2].ToString();
+
+                            _context.Employee.Add(emp);
+                        }
+                        await _context.SaveChangesAsync();
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+            }
+            return View();
         }
     }
 }
--- Models/Process/ExcelProcess.cs
+++ Models/Process/ExcelProcess.cs
@@ -2,78 +2,57 @@ using System.Data;
 using OfficeOpenXml;
 namespace NguyenDuyenCuongBTH3.Models.Process
 {
-public class ExcelProcess
-{
-public DataTable ExcelToDataTable(string strPath)
-{
-FileInfo fi = new FileInfo(strPath);
-ExcelPackage excelPackage = new ExcelPackage(fi);
-DataTable dt = new DataTable() ;
-ExcelWorksheet worksheet = excel Package.Workbook.Worksheets [0] ;
-//check if the worksheet is completely empty
-if (worksheet.Dimension == null)
-{
-return dt;
-}
-//create a list to hold the column names
-List<string> columnNames
-= new Listestring>();
-//needed to keep track of empty column headers
-int currentColumn = 1;
-//loop all columns in the sheet and add them to the datatable
-foreach (var cell in worksheet. Cells[1, 1, 1, worksheet.Dimension.End.Column])
-{
-    string columnName = cell.Text.Trim();
-//check if the previous header was empty and add it if it was
-if (cell.Start.Column != currentColumn)
-{
-columnNames. Add ("Header.
-dt.Columns. Add ("Header
-currentColumn++;
-+
-currentColumn):
-currentColumn):
-7
-//add the column name to the list to count the duplicates
-columnNames. Add (co lumnName);
-//count the duplicate column names and make them unique to avoid the exceptir
-//A column named
-"Name "
-already belongs
-to this DataTable
-int occurrences
-columnNames .Count (x => x. Equals (columnName) ) ;
-if (occurrences
-1)
-{
-columnName
-= columnName
-+ " '+ occurrences;
-7
-//add the column to
-the datatable
-dt. Columns .Add (columnName) ;
-currentColumn++;
-}
-//start adding the contents of the excel file to the datatable
-for (int i
-=2; i
-<2
-worksheet.Dimension.End.Row; it+)
-{
-var row
-= worksheet.Cellsli,
-DataRow newRow = dt.NewRow() :
-//loop all cells in the row
-foreach (var cell in row)
-{
-1.
-i, worksheet.Dimension.End.Column] ;
-newRow[cell.Start.Column
-11 = cell.Text;
-7
-dt. Rows.Add (newRow) ;
-}
-return dt;
-}
+    public class ExcelProcess
+    {
+        public DataTable ExcelToDataTable(string strPath)
+        {
+            FileInfo fi = new FileInfo(strPath);
+            ExcelPackage excelPackage = new ExcelPackage(fi);
+            DataTable dt = new DataTable();
+            ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets[0];
+            if (worksheet.Dimension == null)
+            {
+                return dt;
+            }
+            List<string> columnNames = new List<string>();
+            int currentColumn = 1;
+            foreach (var cell in worksheet.Cells[1, 1, 1, worksheet.Dimension.End.Column])
+            {
+                string columnName = cell.Text.Trim();
+                if (cell.Start.Column != currentColumn)
+                {
+                    columnNames.Add("Header_" + currentColumn);
+                    dt.Columns.Add("Header_" + currentColumn);
+                    currentColumn++;
+                }
+                // thêm tên cột vào danh sách để đếm các bản sao 
+                columnNames.Add(columnName);
+                // đếm các tên cột trùng lặp và đặt chúng thành duy nhất để tránh tồn tại 
+                // Một cột có tên 'Tên' đã thuộc về DataTable này 
+                int occurrences = columnNames.Count(x => x.Equals(columnName));
+                if (occurrences > 1)
+                {
+                    columnName = columnName + "" + occurrences;
+                }
+                // thêm cột vào datatable 
+                dt.Columns.Add(columnName);
+                currentColumn++;
+            }
+            // bắt đầu thêm nội dung của tệp excel vào dữ liệu cho 
+            for (int i = 2; i <= worksheet.Dimension.End.Row; i++)
+            {
+                var row = worksheet.Cells[i, 1, i, worksheet.Dimension.End.Column];
+                DataRow newRow = dt.NewRow();
+                // lặp tất cả các ô trong hàng 
+                foreach (var cell in row)
+                {
+                    newRow[cell.Start.Column - 1] = cell.Text;
+                }
+
+                dt.Rows.Add(newRow);
+            }
+            return dt;
+        }
+    }
+
 }
